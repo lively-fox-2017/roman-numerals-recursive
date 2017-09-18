480cebd728653ef83e9d3b5a46e316ef9729c219
azhariemuhammad <azharie_muhammad@live.com>
2017-09-18 19:17:24 +0700

    roman recursive sudah

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,11 +1,25 @@
-function to_roman(input) {
-  // start your code here
-  return to_roman(input); 
+function to_roman(num) {
+  // your implementation code here
+  let result = "";
+  // yang pertama kita buat arr
+  let angka = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+    for(let i = 0; i < angka.length; i++){
+        if(num == 0){ // base
+            return '';
+        }else if(num % angka[i] < num){ // jika num dimod denga angka[i] dan kurang dari num
+            let a  = num-angka[i]
+          return roman[i]   + to_roman(a) // return roman[i] 
+        }
+    }
+    
 }
 
+// Drive code
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
-console.log('——————|——————————|————————')
+console.log('——————|——————————|———————')
 console.log('4     | IV       | ', to_roman(4))
 console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
