509e18eb379304ba946042af3b88e26dda1cb3ce
lokiloko <ianw.tkj2@gmail.com>
2017-09-18 19:52:48 +0700

    Refactoring to_roman

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -3,63 +3,16 @@ function to_roman(input) {
   if(input === 0){
     return "";
   }
-  if(input - 1000 >= 0){
-    input -= 1000;
-    return 'M' + to_roman(input);
+  var roman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+  var nilai = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+  for(let i =0;i<nilai.length;i++){
+    if(input - nilai[i] >= 0){
+      input -= nilai[i];
+      return roman[i] + to_roman(input);
+    }
   }
-  if(input - 900 >= 0){
-    input -= 900;
-    return 'CM' + to_roman(input);
-  }
-  if(input - 500 >= 0){
-    input -= 500;
-    return 'D' + to_roman(input);
-  }
-  if(input - 400 >= 0){
-    input -= 400;
-    return 'CD' + to_roman(input);
-  }
-  if(input - 100 >= 0){
-    input -= 100;
-    return 'C' + to_roman(input);
-  }
-  if(input - 90 >= 0){
-    input -= 90;
-    return 'XC' + to_roman(input);
-  }
-  if(input - 50 >= 0){
-    input -= 50;
-    return 'L' + to_roman(input);
-  }
-  if(input - 40 >= 0){
-    input -= 40;
-    return 'XL' + to_roman(input);
-  }
-  if(input - 10 >= 0){
-    input -= 10;
-    return 'X' + to_roman(input);
-  }
-  if(input - 9 >= 0){
-    input -= 9;
-    return 'IX' + to_roman(input);
-  }
-  if(input - 5 >= 0){
-    input -= 5;
-    return 'V' + to_roman(input);
-  }
-  if(input - 4 >= 0){
-    input -= 4;
-    return 'IV' + to_roman(input);
-  }
-  if(input - 1 >= 0){
-    input -= 1;
-    return 'I' + to_roman(input);
-  }
-
-  return to_roman(input);
 }
 
-console.log(to_roman(999))
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|————————')
