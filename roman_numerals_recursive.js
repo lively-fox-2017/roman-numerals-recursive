dc8407ee74c665ce9527e1df086cab0a5086726e
jasoneffendi <jasoneffendi27@gmail.com>
2017-09-18 14:36:51 +0700

    Roman Numeral Recursive Done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,13 +1,34 @@
-function to_roman(input) {
-  // start your code here
-  return to_roman(input); 
-}
+function to_roman(num) {
+  var roman = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I']
+  ];
 
+  if (num === 0) {
+    return '';
+  }
+  for (var i = 0; i < roman.length; i++) {
+    if (num >= roman[i][0]) {
+      return roman[i][1] + to_roman(num - roman[i][0]);
+    }
+  }
+}
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|————————')
-console.log('4     | IV       | ', to_roman(4))
-console.log('9     | IX       | ', to_roman(9))
+// console.log('4     | IV       | ', to_roman(4))
+// console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
-console.log('1453  | MCDLIII  | ', to_roman(1453))
-console.log('1646  | MDCXLVI  | ', to_roman(1646))
+// console.log('1453  | MCDLIII  | ', to_roman(1453))
+// console.log('1646  | MDCXLVI  | ', to_roman(1646))
