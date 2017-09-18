f00eb69e66d86e2e26e7f69349145404a67d7ca7
= <pangkeyd@gmail.com>
2017-09-18 22:14:02 +0700

    roman-numerals-recursive-dpangkey

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,23 @@
 function to_roman(input) {
   // start your code here
-  return to_roman(input); 
+  var res = ''
+  var latin = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+  var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
+
+  var numArrRom = ['M', 'D', 'CD', 'C', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+  var numArrLat = [1000, 500, 400, 100, 50, 40, 10, 9, 5, 4, 1]
+  var tampung = ''
+
+  for(var i = 0; i < numArrLat.length; i++){
+  	if(input <= 0){
+  		return tampung;
+  	}else if(input >= numArrLat[i]){
+  		tampung += numArrRom[i]
+  		input -= numArrLat[i]
+  	}
+  }
+  return tampung + to_roman(input)
+  
 }
 
 console.log('My totally sweet testing script for new roman\n')
