237f9afa875a5338cf110bbf1379e2cdc608e45a
febrimailand <febri.mailand@gmail.com>
2017-09-18 15:19:44 +0700

    challenge roman-numerals-recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,7 +1,27 @@
 function to_roman(input) {
   // start your code here
-  return to_roman(input); 
+  var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+  var latin = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 ]
+
+
+  var tampung = ''
+
+  for (i = latin.length-1; i >= 0; i--){
+    if(input <= 0){
+      return tampung;
+    } else {
+      if (input >= latin[i]){
+        tampung +=roman[i];
+        input -= latin[i];
+        return tampung + to_roman(input)
+      }
+    }
+  }
+
+
+  return to_roman(input);
 }
+
 
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
