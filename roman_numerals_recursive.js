d4d484cd2b877e4205ea493e4014bf94247d5e95
nach9 <nur.achdiansyah@gmail.com>
2017-09-18 16:39:08 +0700

    Done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,34 @@
 function to_roman(input) {
   // start your code here
-  return to_roman(input); 
+  var numS = input.toString(); // merubah num jadi string
+  var romawi ='';
+  var roman = [[0,'',''],[1,'I','V'] ,[2 , 'X' ,'L'] ,[3,'C','D'] , [4,'M' , 'V']] ;
+
+  var unit = numS.length;
+  if ('123'.indexOf(numS[0])!=-1){
+    romawi = roman[unit][1].repeat(numS[0]) ;
+  }
+  else if('678'.indexOf(numS[0])!=-1){
+    romawi = roman[unit][2] + roman[unit][1].repeat(numS[0]-5) ;
+  }
+  else if (numS[0]==4 ){
+    romawi = roman[unit][1]+roman[unit][2]  ;
+  }
+  else if(numS[0]==5){
+    romawi = roman[unit][2]  ;
+  }
+  else if(numS[0]==9){
+    romawi = roman[unit][1]+roman[unit+1][1]  ;
+  }
+
+  if (numS.length==1){
+    return romawi;
+  }
+  else{
+    return romawi += to_roman(Number(numS.substring(1,999)));
+  }
+
+
 }
 
 console.log('My totally sweet testing script for new roman\n')
